de6d6415937d3f8a815f3e5c4b46c4f612b4705a
Beth Barnes <bethcbarnes@outlook.com>
2018-06-23 10:13:38 -0400

    feat(EventForm): add form

--- client/components/event-form.js
+++ client/components/event-form.js
@@ -8,14 +8,42 @@ class EventForm extends Component {
   constructor(props){
     super(props)
     this.state = {
-      test: true
+      addButtonClicked: false
     }
   }
+
+  handleAddEventClick = (event) => {
+    console.log('clicked add event')
+    this.setState({
+      addButtonClicked: true
+    })
+  }
+
+  // maybe the button should be in the other component and render this form component if clicked
 
   render(){
     return (
       <div>
-        Helloooooo
+        <button onClick={this.handleAddEventClick}>
+        add event
+        </button>
+        {this.state.addButtonClicked ?
+        <form>
+          <label>
+            New Event:
+          </label>
+          Event Title:
+          <input type="text" name="title"/>
+          Description:
+          <input type="text" name="description"/>
+          {/* start time end time */}
+
+        </form>
+
+
+        :
+        <div/>}
+
         </div>
     )
   }
